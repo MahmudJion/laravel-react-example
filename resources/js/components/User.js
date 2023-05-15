6db0740e7882545a43d94b8e0393d1f55d2590b1
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-05-15 23:49:19 +0100

    update User.js

--- resources/js/components/User.js
+++ resources/js/components/User.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function User() {
+const User = () => {
     return (
         <div className="container mt-5">
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card text-center">
                         <div className="card-header"><h2>React in Laravel</h2></div>
-
-                        <div className="card-body">I'm a React component in Laravel app!</div>
+                        <div className="card-body">I'm a React component in a Laravel app!</div>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default User;
 
 // DOM element
-if (document.getElementById('user')) {
-    ReactDOM.render(<User />, document.getElementById('user'));
+const rootElement = document.getElementById('user');
+if (rootElement) {
+    ReactDOM.render(<User />, rootElement);
 }
